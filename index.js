0259fa73649d7dff3c14e1abbac819bd470333c9
Michael Newman <therealmichaelnewman@gmail.com>
2021-07-30 14:04:44 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+    
+    input.addEventListener('click', clickAlert); 
+
 }
+
+addingEventListener();
